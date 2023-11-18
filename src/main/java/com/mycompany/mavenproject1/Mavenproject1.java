be996b7ae9ce7d07046b45a2bfe6ff1cfbcccb45
Akash2856 <akashgupta2856@gmail.com>
2023-11-19 00:37:14 +0530

    spring.xml file is present in src->main->resource; ApplicationContext factory = new ClassPathXmlApplicationContext(spring.xml);

--- src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -6,6 +6,8 @@ package com.mycompany.mavenproject1;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 /**
@@ -16,7 +18,8 @@ public class Mavenproject1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        BeanFactory factory =new XmlBeanFactory(new  FileSystemResource("spring.xml"));
+        //BeanFactory factory =new XmlBeanFactory(new  FileSystemResource("spring.xml"));
+        ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");
         Alien obj = (Alien) factory.getBean("Alien");
         obj.code();
     }
